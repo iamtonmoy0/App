52c2e6a77ec2c755f7a6c834e1b67e2c287ea4d9
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-03 06:43:28 -0400

    ui modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,6 @@ import React, { Component } from 'react';
 export const Header=(props)=>{
 
 	return(
-		<h2>Task list {props.title} </h2>
+		<h2>Task list </h2>
 	)
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,7 +3,7 @@ import './TaskCard.css'
 
 export const TaskCard=({task,handleDel})=>{
 	return(
-			<li >
+			<li className='list'>
 
 				<span>{task.name}</span>- <span>{task.id}</span>
 				<button className="del " onClick={()=>handleDel(task.id)}>delete </button> 
