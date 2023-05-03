0f40a4e1f66f270b52d13e334d3be279731f55d0
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-03 10:07:02 -0400

    reset option added

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 
 export const AddTask = () => {
+
+	const [TaskValue,setTaskValue]=useState('');
+	const handleChange=(e)=>{
+		setTaskValue(e.target.value)
+	}
+	const handleRes=()=>{
+		setTaskValue("");  //resetting the task value
+	}
 	return (
 		<div className="Task">
 			<form action="">
 				<label htmlFor="task"> Task Name:</label>
-				<input type="text"  name="task" id="task" placeholder="Enter Your task" autoComplete="off"/>
-				<button type="submit" style={{backgroundColor:"tomato"}}> Add task</button>
+				<input type="text"  name="task" id="task" placeholder="Enter Your task" autoComplete="off" onChange={handleChange} value={TaskValue}/>
+				<button type="submit" style={{backgroundColor:"green"}}> Add task</button>
+				<button type="submit" style={{backgroundColor:"tomato"}} onClick={handleRes}> Reset</button>
 			</form>
+			{TaskValue.length}
 			
 		</div>
 	);
